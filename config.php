aca92818e51b48094f3e5b58ec5dda74c03917fe
Mathias <mathias.lavergne.graph@gmail.com>
2019-06-10 19:20:28 +0200

    Ajout du fichier config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+date_default_timezone_set('Europe/Paris');
+
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+    define('HOST', 'https://' . $_SERVER['HTTP_HOST'] . '/projet_05/');
+} else {
+    define('HOST', 'http://' . $_SERVER['HTTP_HOST'] . '/projet_05/');
+}
+
+define('ROOT', __DIR__ . DIRECTORY_SEPARATOR); // DIR is the folder where we can find the file calling the constant
+
+define('CORE', ROOT . 'src' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR);
+define('LOG', ROOT . 'logs' . DIRECTORY_SEPARATOR);
+
+define('VIEWFRONT', ROOT . 'view' . DIRECTORY_SEPARATOR . 'frontend' . DIRECTORY_SEPARATOR);
+define('VIEWBACK', ROOT . 'view' . DIRECTORY_SEPARATOR . 'backend' . DIRECTORY_SEPARATOR);
+define('TEMPLATE', ROOT . 'view' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR);
+define('ASSETS', HOST . 'public/');
