40ee6ea4203f296b22e0d805b4faafbcee2f93c0
Mathias <mathias.lavergne.graph@gmail.com>
2019-06-10 19:26:24 +0200

    Ajout de la classe HomeController.php

---
+++ src/controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Model\GameManager;
+use App\Model\Pagination;
+use App\Core\View;
+// use App\Controller\ConnectionController;
+
+/**
+ * HomeController
+ * 
+ * Allows to show the home
+ */
+
+class HomeController
+{
+    /**
+     * Allows to show the home
+     * 
+     * @param array $params optionnal
+     */
+    public function showHome($params = [])
+    {   
+        
+        $gameManager = new GameManager();
+        
+        $games = $gameManager->listGames();
+
+        $view = new View('home');
+        $view->render('front', array(
+            'games' => $games));
+
+    }
+}
+
+
