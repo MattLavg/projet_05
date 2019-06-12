7a57b99530a8303c44d645f8d565283dd9159f43
Mathias <mathias.lavergne.graph@gmail.com>
2019-06-12 23:36:33 +0200

    Ajout du manager GameManager.php

---
+++ src/model/GameManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Model;
+
+use App\Core\Manager;
+use App\Model\Game;
+
+/**
+ *  GameManager
+ * 
+ *  Allows to list, count, get, add, update and delete games
+ */
+
+ class GameManager extends Manager
+ {
+    /**
+     * @var string $_table
+    */
+    protected $_table = 'games';
+
+    /**
+     * @var string $_class
+    */
+    protected $_class = 'App\Model\Game';
+
+
+    /**
+     * Allows to get a game
+     * 
+     * @param int $id
+     * @return object PDOStatement
+     */
+    public function getGame($id)
+    { 
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT * FROM games WHERE id = ?');
+        $req->execute(array($id));
+
+        $data = $req->fetch(\PDO::FETCH_ASSOC);
+
+        // if (!$data) {
+        //     throw new MyException('Impossible d\'afficher l\'article');
+        // }
+
+        $game = new Game();
+        $game->hydrate($data);
+
+        return $game;
+    }
+ }
