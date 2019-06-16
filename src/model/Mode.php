ade2c37268744714b69b24731f1b7401cb7fa990
Mathias <mathias.lavergne.graph@gmail.com>
2019-06-16 22:50:42 +0200

    Ajout de la classe Mode.php

---
+++ src/model/Mode.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Model;
+
+/**
+ * Mode
+ * 
+ * Set or get informations for a mode
+ */
+
+class Mode
+{
+    /**
+     * @var int $_id, the id of a mode
+     */
+    protected $_id;
+
+    /**
+     * @var string $_name, the name of a mode
+     */
+    protected $_name;
+
+    /**
+     * Set automatically elements in methods
+     * 
+     * @param array $data
+     */
+    public function hydrate($data)
+    {   
+        foreach ($data as $key => $value) {
+
+            $method = 'set' . ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    // GETTERS
+
+    /**
+     * Allows to get the id of a mode
+     * 
+     * @return int $_id
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Allows to get the name of a mode
+     * 
+     * @return string $_name
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    // SETTERS
+
+    /**
+     * Allows to set the id of a mode
+     * 
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->_id = $id;
+    }
+
+    /**
+     * Allows to set the name of a mode
+     * 
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->_name = $name;
+    }
+
+}
