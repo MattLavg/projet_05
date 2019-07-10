c8c7055efe1266545a13369c0bbc1356ffde29f8
Mathias <mathias.lavergne.graph@gmail.com>
2019-07-10 19:37:42 +0200

    Ajout du CommentController.php

---
+++ src/controller/CommentController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\CommentManager;
+use App\Core\View;
+
+/**
+ *  CommentController
+ * 
+ *  Allows to add, report and delete comments
+ */
+
+class CommentController
+{
+    /**
+     * Allows to add comment
+     * 
+     * @param array $params
+     */
+    public function addComment($params = [])
+    {
+        echo "<pre>";
+        print_r($params);
+        echo "</pre>";die;
+
+        $params['author'] = trim(strip_tags($params['author']));
+        $params['content'] = trim(strip_tags($params['content']));
+
+        if (!empty($params['author']) && !empty($params['content'])) {
+
+            $commentManager = new CommentManager();
+
+            if (ConnectionController::isSessionValid()) {
+                $commentId = $commentManager->addComment($params, $admin = true);
+            } else {
+                $commentId = $commentManager->addComment($params);
+            }
+
+            if ($commentId) {
+                $_SESSION['actionDone'] = 'Votre commentaire a bien été publié.';
+            }
+
+            $view = new View();
+            $view->redirect('game/id/' . $params['game-id'] . '#anchorGame');
+
+        } else {
+
+            $_SESSION['errorMessage'] = 'Vous devez renseigner tous les champs du formulaire.';
+
+            $view = new View();
+            $view->redirect('game/id/' . $params['game-id'] . '#anchorGame');
+        }
+    }
+
+    /**
+     * Allows to delete comment
+     * 
+     * @param array $params
+     */
+    public function deleteComment($params)
+    { 
+        $commentManager = new CommentManager();
+        $commentManager->deleteComment($params['id']);
+
+        $_SESSION['actionDone'] = 'Vous avez supprimé un commentaire.';
+
+        if (isset($params['game-id'])) {
+            $view = new View();
+            $view->redirect('game/id/' . $params['game-id'] . '#anchorGame');
+        } else {
+            $view = new View();
+            $view->redirect('reported-comments');
+        }
+    }
+
+    /**
+     * Allows to report comment
+     * 
+     * @param array $params
+     */
+    public function reportComment($params)
+    {
+        $commentManager = new CommentManager();
+        $isReportedComment = $commentManager->isReportedComment($params['id']);
+
+        if ($isReportedComment['reported'] == 1) {
+
+            $_SESSION['actionDone'] = 'Le commentaire a déjà été signalé.';
+
+        } else {
+
+            $reportedComment = $commentManager->reportComment($params['id']);
+
+            if ($reportedComment) {
+                $_SESSION['actionDone'] = 'Le commentaire a bien été signalé.';
+            }
+
+        }
+
+        $view = new View();
+        $view->redirect('game/id/' . $params['game-id'] . '#anchorGame');
+    }
+
+    /**
+     * Allows to validate comment
+     * 
+     * @param array $params
+     */
+    public function validComment($params)
+    {
+        $commentManager = new CommentManager();
+        $commentManager->validComment($params['id']);
+
+        $_SESSION['actionDone'] = 'Le commentaire a été publié.';
+
+        if (isset($params['game-id'])) {
+            $view = new View();
+            $view->redirect('game/id/' . $params['game-id'] . '#anchorGame');
+        } else {
+            $view = new View();
+            $view->redirect('reported-comments');
+        }
+    }
+}
